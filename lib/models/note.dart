ed05be80c4be939129c5f2909b370fd03d2eef8f
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-01-27 15:04:54 +0545

    Add models note

---
+++ lib/models/note.dart
@@ -0,0 +1,20 @@
+// External Import
+import 'package:flutter/material.dart';
+
+class Note {
+  final int id;
+  final String title;
+  final String content;
+  final DateTime dateCreated;
+  final DateTime dateUpdated;
+  bool isFavorite = false;
+
+  Note({
+    @required this.id,
+    @required this.title,
+    @required this.content,
+    @required this.dateCreated,
+    @required this.dateUpdated,
+    this.isFavorite,
+  });
+}
