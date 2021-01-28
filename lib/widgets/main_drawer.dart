e14221d7abc8fa07523aae81841a2751b4e80e1f
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-01-28 18:37:48 +0545

    Working on drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,6 +1,7 @@
 // External Import
 import 'package:flutter/material.dart';
 import 'package:omit/screens/tabs_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // Internal Import
 import '../constants.dart';
@@ -9,43 +10,66 @@ import '../screens/settings_screen.dart';
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          AppBar(
-            title: Text('Omit'),
-            automaticallyImplyLeading: false,
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(
-              Icons.note,
-              color: Theme.of(context).accentColor,
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'OMIT',
+                    style: GoogleFonts.londrinaSolid(
+                      color: Theme.of(context).accentColor,
+                      fontSize: 60.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 5,
+                    ),
+                  ),
+                  Text(
+                    'Clear Your Mind',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.baloo(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
             ),
-            title: Text(
-              'Notes',
-              style: kAppBarListTextStyle,
+            ListTile(
+              leading: Icon(
+                Icons.note,
+                color: Theme.of(context).accentColor,
+              ),
+              title: Text(
+                'Notes',
+                style: kAppBarListTextStyle,
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .pushReplacementNamed(TabsScreen.routeName);
+              },
             ),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed(TabsScreen.routeName);
-            },
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(
-              Icons.settings,
-              color: Theme.of(context).accentColor,
+            Divider(),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Theme.of(context).accentColor,
+              ),
+              title: Text(
+                'Settings',
+                style: kAppBarListTextStyle,
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .pushReplacementNamed(SettingScreen.routeName);
+              },
             ),
-            title: Text(
-              'Settings',
-              style: kAppBarListTextStyle,
-            ),
-            onTap: () {
-              Navigator.of(context)
-                  .pushReplacementNamed(SettingScreen.routeName);
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
